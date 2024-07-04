5c20b623805f514e25201ca0c67ec7e9c09ea847
Aviv Bar Or <92517487+avivbaror@users.noreply.github.com>
2024-07-04 18:23:21 +0300

    Add files via upload

---
+++ kmeans_clustering.c
@@ -0,0 +1,23 @@
+# include <stdio.h>
+# include <stdlib.h>
+# include <math.h>
+
+/* 
+float* K_means(K, iter=200, input_data.txt){
+    make_sure_input_is_valid() 
+    f = open(input_data.txt)
+    compute_d(f) #number of columms
+    put_in_matrix size N*d #use %4f to make sure inputs/outputs are at most 4 decimal places.
+
+
+    init_initial_centroids(matrix) (inital centroids(matrix of size k*d) as first k data points
+
+    While (convergence(centroids) OR iteration_number > iter:
+        for each data_point:
+            find_closest_centroid(x,centroids)
+        
+        update_centroids (u_k= mean(u))
+    
+    return centroids
+
+ */
