d6bb473eff5ee52dfd76d7c13111c4274b20dea5
Aviv Bar Or <92517487+avivbaror@users.noreply.github.com>
2024-07-06 21:27:17 +0300

    Add files via upload

--- kmeans_clustering.c
+++ kmeans_clustering.c
@@ -1,6 +1,7 @@
-# include <stdio.h>
-# include <stdlib.h>
-# include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
 
 /* 
 float* K_means(K, iter=200, input_data.txt){
@@ -22,27 +23,51 @@ float* K_means(K, iter=200, input_data.txt){
 
  */
 
-double* k_means()
+int int k_means(int k, int iter, char* filename);
 void is_valid_input(int k, int iter);
+int compute_dimension(char *fp);
 
 int main(int argc, char** argv){
+    char *filename = "input_data.txt"; //argv[2]
     int k, iter;
-    char* filename;
-    k = //argv[0].to_int()
-    iter =//argv[1] check if it is an integer if not. we take iter=200
-    filename = //argv[2]
-    is_valid_input(k, iter, filename);
+    k = 3; //argv[0].to_int()
+    iter = 200; //argv[1] check if it is an integer if not. we take iter=200
+    //is_valid_input(k, iter, filename);
 
-
-    return k_means(k, iter, filename)
+    return k_means(k, iter, filename);
 }
 
-double* k_means(int k, int iter, char* filename){
+
+int k_means(int k, int iter, char* filename){
+    int n;
+    int d = compute_dimension(filename);
     FILE *fp = fopen(filename, "r");
     if (fp ==NULL){
-        printf("Error: could not open file %s", filename);
+        perror("Failed to read line (line 57 in the code)");
+        fclose(fp);
         return 1;
     }
-
+    fclose(fp);
+    return 0;
 }
+
+/* calculating d which is the size of each data point */
+int compute_dimension(char *filename){
+    FILE *fp = fopen(filename, "r");
+     if (fp ==NULL){
+        perror("Failed to read line (line 57 in the code)");
+        fclose(fp);
+        exit(1);
+    }
+    int cnt = 0;
+    char ch;
+    while((ch = fgetc(fp)) != '\n'){
+       if (ch == ',')
+       cnt++; 
+    }
+    fclose(fp);
+    return cnt + 1; // Number of "," + 1
+}
+
+
 
