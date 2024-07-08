1be1138d64047af3bb7fa0766dea8d206f5c1e72
Aviv Bar Or <92517487+avivbaror@users.noreply.github.com>
2024-07-08 19:46:14 +0300

    Add files via upload

--- kmeans_clustering.c
+++ kmeans_clustering.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <string.h>
 
 /* PLAN:
 float* K_means(K, iter=200, input_data.txt){
-    make_sure_input_is_valid() 
-    f = open(input_data.txt)
-    compute_d(f) #number of columms
-    put_in_matrix size N*d #use %4f to make sure inputs/outputs are at most 4 decimal places.
+    make_sure_input_is_valid()  
+    compute_d(f) #number of columms (V)
+    compute n (V)
+    put_in_matrix size N*d #use %4f to make sure inputs/outputs are at most 4 decimal places. (V)
 
     init_initial_centroids(matrix) (inital centroids(matrix of size k*d) as first k data points
 
@@ -27,49 +26,16 @@ double** compute_data_matrix(char *filename, int n, int d);
 int compute_d(char *filename);
 int compute_n(char *filename);
 double** initial_centroids(double** mat, int k, int d);
+// for testings:
+void print_matrix(double** matrix, int n, int d);
 
-
-int k_means(int k, int iter, char* filename){
-    int d = compute_d(filename);
-    int n = compute_n(filename);
-    double **data_matrix, **centroids;
-
-    data_matrix = compute_data_matrix(filename, n, d);
-    
-    centroids = initial_centroids(data_matrix, k, d);
-
-    //Print matrix - just to check correctness of code  
-    printf("printing whole matrix:\n");
-    for(int i=0; i<n; i++){
-        for(int j=0;j<d;j++){
-            printf("%.4f ", data_matrix[i][j]);
-        }
-        printf("\n");
-    }
-    
-    //Print k centroids - just to check correctness of code
-    printf("printing whole matrix:\n");  
-    for(int i=0; i<n; i++){
-        for(int j=0;j<d;j++){
-            printf("%.4f ", centroids[i][j]);
-        }
-        printf("\n");
-    }
-    
-    //Free the allocated memory
-    for (int i = 0; i < n; i++) {
-        free(data_matrix[i]);
-    }
-    free(data_matrix);
-    return 0;
-}
 /* Calculating d - vector size */
 int compute_d(char *filename){
     FILE *fp = fopen(filename, "r");
     int d = 0;
     char ch;
      if (fp ==NULL){
-        perror("Failed to read line (line 61 in the code)");
+        perror("Failed to read line (line 35 in the code)");
         exit(1);
     }
     while((ch = fgetc(fp)) != '\n'){
@@ -87,7 +53,7 @@ int compute_n(char *filename){
     int n = 0;
     char line[255];
     if (fp ==NULL){
-        perror("Failed to read line (line 79 in the code)");
+        perror("Failed to read line (line 53 in the code)");
         exit(1);
     }
      while (fgets(line, 255, fp) != NULL) {
@@ -98,7 +64,20 @@ int compute_n(char *filename){
     return n;
 }
 
-/* reads a text file with n rows and d columns, converts it into a double matrix of the size n*d */
+/**
+ * @brief Reads a file and stores data points in a matrix.
+ *
+ * This function reads a file containing `n` rows and `d` columns of data points,
+ * separated by commas. It allocates memory for an n*d matrix and fills it with 
+ * the data points. If memory allocation fails or the file can't be opened, it prints an 
+ * error and exits.
+ *
+ * @param filename The name of the file to read.
+ * @param n The number of rows.
+ * @param d The number of columns.
+ * 
+ * @return A pointer to the allocated matrix.
+ */
 double** compute_data_matrix(char *filename, int n, int d){
     FILE *fp;
     int i,j, buffer_index, k;
@@ -107,19 +86,19 @@ double** compute_data_matrix(char *filename, int n, int d){
     char buffer[50];
     char ch;
     if (mat == NULL){
-        perror("Failed to allocate memory (line 99)");
+        perror("Failed to allocate memory (line 73)");
         exit(1);
     }
     for (i=0; i<n; i++){
         mat[i] = malloc(sizeof(double) * d);
         if (mat[i] == NULL){
-            perror("Failed to allocate memory (line 105)");
+            perror("Failed to allocate memory (line 79)");
             exit(1);
         }
     }
     fp = fopen(filename, "r");
     if (fp ==NULL){
-        perror("Failed to read line (line 111)");
+        perror("Failed to read line (line 85)");
         exit(1);
     }
     i=0;
@@ -142,6 +121,7 @@ double** compute_data_matrix(char *filename, int n, int d){
         }
         i++;
     }
+
     fclose(fp);
     return mat;
 }
@@ -151,7 +131,7 @@ double** initial_centroids(double** mat, int k, int d){
     for (int i=0; i<k; i++){
         centroids[i] = malloc(sizeof(double) * d);
         if (centroids[i] == NULL){
-            perror("Failed to allocate memory (line 105)");
+            perror("Failed to allocate memory (line 134)");
             exit(1);
         }
         for (int j=0; j<d; j++){
@@ -161,12 +141,63 @@ double** initial_centroids(double** mat, int k, int d){
     return centroids;
 }
 
-int main(int argc, char** argv){
-    int k = atoi(argv[1]); //later check validity
-    int iter = atoi(argv[2]); //later check validity
-    char* filename = argv[3]; //assume validity
-    printf("iter: %d\n", iter);
-    k_means(k, iter, filename);
-    
+int k_means(int k, int iter, char* filename){
+    int i, j;
+    int d = compute_d(filename);
+    int n = compute_n(filename);
+    double **data_matrix, **centroids, **clusters;
+    int *vectors_per_clusters;
+
+    data_matrix = compute_data_matrix(filename, n, d);
+    centroids = initial_centroids(data_matrix, k, d);
+
+    print_matrix(data_matrix, k, d);
+
+    //Free the allocated memory
+    for (i = 0; i < n; i++) {
+        free(data_matrix[i]);
+    }
+    free(data_matrix);
+    for (i = 0; i < k; i++){
+        free(centroids[i]);
+    }
+    free(centroids);
+
     return 0;
+}
+
+
+/*
+Missing:
+* valid inputs of extreme cases */
+int main(int argc, char** argv){
+    int k, iter; //later check validity
+    char *filename;
+    if ((argc != 3) && (argc != 4)){
+        perror("Invalid Input!");
+        return 1;
+    }
+    else if (argc == 4){
+        int k = atoi(argv[1]); 
+        int iter = atoi(argv[2]); 
+        filename = argv[3];
+    }
+    else if (argc == 3){
+        k = atoi(argv[1]); 
+        iter = 200;
+        filename = argv[2];
+    }
+    k_means(k, iter, filename);
+    return 0;
+}
+   
+//Print matrix - just to check correctness of code  
+void print_matrix(double** matrix, int n, int d){
+    int i, j;
+    for(i=0; i<n; i++){
+        for(j=0;j<d;j++){
+            printf("%.4f ", matrix[i][j]);
+        }
+        printf("\n");
+    }
 }
