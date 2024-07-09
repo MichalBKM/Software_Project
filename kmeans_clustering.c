b4aa7e8f3c3aad977486c7d5b68fc7b34a052def
Aviv Bar Or <92517487+avivbaror@users.noreply.github.com>
2024-07-09 10:26:15 +0300

    Add files via upload
    functions added:
    find_closest_point
    vector_distance
    alloc_memory_to_matrix

--- kmeans_clustering.c
+++ kmeans_clustering.c
@@ -25,7 +25,10 @@ void is_valid_input(int k, int iter);
 double** compute_data_matrix(char *filename, int n, int d);
 int compute_d(char *filename);
 int compute_n(char *filename);
-double** initial_centroids(double** mat, int k, int d);
+double** copy_first_k_elements(double** read_matrix, double**write_matrix int k, int d);
+void free_matrix(double** matrix, int n);
+int convergence(double** centroids, double** before, int curr_iter, int max_iter, int k, int d);
+double** alloc_memory_to_matrix(double** matrix, int rows, int columns, const char *error_msg);
 // for testings:
 void print_matrix(double** matrix, int n, int d);
 
@@ -35,7 +38,7 @@ int compute_d(char *filename){
     int d = 0;
     char ch;
      if (fp ==NULL){
-        perror("Failed to read line (line 35 in the code)");
+        perror("Failed to read line (line 39 in the code)");
         exit(1);
     }
     while((ch = fgetc(fp)) != '\n'){
@@ -53,7 +56,7 @@ int compute_n(char *filename){
     int n = 0;
     char line[255];
     if (fp ==NULL){
-        perror("Failed to read line (line 53 in the code)");
+        perror("Failed to read line (line 57 in the code)");
         exit(1);
     }
      while (fgets(line, 255, fp) != NULL) {
@@ -67,10 +70,9 @@ int compute_n(char *filename){
 /**
  * @brief Reads a file and stores data points in a matrix.
  *
- * This function reads a file containing `n` rows and `d` columns of data points,
+ * This function reads an input file containing `n` rows and `d` columns of data points,
  * separated by commas. It allocates memory for an n*d matrix and fills it with 
- * the data points. If memory allocation fails or the file can't be opened, it prints an 
- * error and exits.
+ * the data points.
  *
  * @param filename The name of the file to read.
  * @param n The number of rows.
@@ -81,24 +83,13 @@ int compute_n(char *filename){
 double** compute_data_matrix(char *filename, int n, int d){
     FILE *fp;
     int i,j, buffer_index, k;
-    double** mat = malloc(sizeof(double*) * n);
+    double** mat = alloc_memory_to_matrix(mat, n, d, "Failed to allocate memory to data matrix (line 85)");
     char line[255];
     char buffer[50];
     char ch;
-    if (mat == NULL){
-        perror("Failed to allocate memory (line 73)");
-        exit(1);
-    }
-    for (i=0; i<n; i++){
-        mat[i] = malloc(sizeof(double) * d);
-        if (mat[i] == NULL){
-            perror("Failed to allocate memory (line 79)");
-            exit(1);
-        }
-    }
     fp = fopen(filename, "r");
     if (fp ==NULL){
-        perror("Failed to read line (line 85)");
+        perror("Failed to read line (line 101)");
         exit(1);
     }
     i=0;
@@ -126,45 +117,94 @@ double** compute_data_matrix(char *filename, int n, int d){
     return mat;
 }
 
-double** initial_centroids(double** mat, int k, int d){
+/**
+ * @brief Copies the first k rows from a matrix to a new k*d matrix.
+ *
+ * This function copies the first `k` rows from the given matrix
+ * with `d` columns into another matrix of size `k*d` at least.
+ * It returns a pointer to the new matrix.
+ *
+ * @param read_matrix The matrix from which you read.
+ * @param write_matrix The matrix to which you write.
+ * @param k The number of rows to copy.
+ * @param d The number of columns per row.
+ * 
+ * @return A pointer to the new k*d matrix.
+ */
+double** copy_first_k_elements(double** read_matrix, double**write_matrix, int k, int d){
     int i, j;
-    double** centroids;
-    printf("&&\n");
-    centroids = malloc(sizeof(double*) * k);
-
-    if (centroids == NULL){
-        perror("Failed to allocate memory (line 73)");
-        exit(1);
-    }
-    printf("allocated memory 1 OK\n");
-
-    for (i=0; i<k; i++){ //failed here - why????
-        centroids[i] = malloc(sizeof(double) * d);
-        if (centroids[i] == NULL){
-            perror("Failed to allocate memory (line 79)");
-            exit(1);
-            }
-        printf("allocated memory 2 OK\n");
-
-        for (j=0; j<d; j++){
-            printf("$");
-            centroids[i][j] = mat[i][j];
-            printf("centroids: %f",centroids[i][j]);
+    for (i =0; i< k; i++){
+        for (int j=0; j<d; j++){
+            write_matrix[i][j] = read_matrix[i][j];
         }
     }
-    return centroids;
+    return write_matrix;
 }
-    
-
-//Print matrix - for testings - works OK
-void print_matrix(double** matrix, int rows, int cols){
+   
+//Print matrix - just to check correctness of code  
+void print_matrix(double** matrix, int n, int d){
     int i, j;
-    for(i=0; i<rows; i++){
-        for(j=0;j<cols;j++){
+    for(i=0; i<n; i++){
+        for(j=0;j<d;j++){
             printf("%.4f ", matrix[i][j]);
         }
         printf("\n");
     }
+}
+
+//Free the allocated memory
+void free_matrix(double** matrix, int n){
+    int i;
+    for (i = 0; i < n; i++) {
+        free(matrix[i]);
+    }
+    free(matrix);
+    
+}
+
+double vector_distance(double *x, double *y, int d){
+    double res;
+    int i;
+    res = 0;
+    for(i = 0; i < d; i++){
+        res += pow((x[i]-y[i]), 2);
+    }
+    res = pow(res, 0.5);
+    return res;
+}
+
+void find_closest_point(double* vector, double** centroids, int* vectors_per_cluster, double **clusters, int d, int k){
+    int i, j, min_index;
+    double dist;
+    double min = INFINITY;
+    for (i=0; i<k ; i++){
+        dist = vector_distance(vector, centroids[i], d);
+        if (dist < min){
+            min_index = i;
+            min = dist;
+        }
+    }
+    vectors_per_cluster[min_index]++;
+    for(j=0; j<d; j++){
+      clusters[min_index][j]+= vector[j];
+    }
+}
+
+double** alloc_memory_to_matrix(double** matrix, int rows, int columns, const char *error_msg){
+    int i;
+    double **matrix = malloc(sizeof(double*) * rows);
+    if (matrix == NULL){
+        perror(error_msg);
+        exit(1);
+    }
+    for (i=0; i<rows; i++){
+        matrix[i] = malloc(sizeof(double) * columns);
+        if (matrix[i] == NULL){
+            perror(error_msg);
+            exit(1);
+        }
+    }
+    return matrix;
 }
 
 int k_means(int k, int iter, char* filename){
@@ -172,30 +212,33 @@ int k_means(int k, int iter, char* filename){
     int d = compute_d(filename);
     int n = compute_n(filename);
     double **data_matrix, **centroids, **clusters;
-    int *vectors_per_clusters;
+    int *vectors_per_cluster;
+
+    //allocate memory for the centroids matrix
+    centroids = alloc_memory_to_matrix(centroids, k, d, "Failed to allocate memory to centroids matrix");
+
+    //allocate memory for the cluster matrix
+    clusters = alloc_memory_to_matrix(clusters, k, d), "Failed to allocate memory to clusters matrix";
+
+    //allocate memory for the vectors_per_cluster matrix
+    vectors_per_cluster = malloc(sizeof(int) * k);
+    if (vectors_per_cluster == NULL) {
+        perror("Failed to allocate memory to vectors_per_cluster matrix")
+        exit(1);
+    }
+
+
 
     data_matrix = compute_data_matrix(filename, n, d);
-    centroids = initial_centroids(data_matrix, k, d); //FAILING
+    centroids = copy_first_k_elements(data_matrix, centroids, k, d);
 
-    //printf("matrix:\n");
-    //print_matrix(data_matrix, n, d);
-    printf("centroids:\n");
-    print_matrix(centroids, k, d);
 
-    //Free the allocated memory
-    /*
-    for (i = 0; i < n; i++) {
-        free(data_matrix[i]);
-    }
-    free(data_matrix);
-    for (i = 0; i < k; i++){
-        free(centroids[i]);
-    }
-    free(centroids);*/
+    free_matrix(data_matrix, n);
+
+    print_matrix(centroids);
 
     return 0;
 }
-
 
 /*
 Missing:
@@ -203,19 +246,19 @@ Missing:
 int main(int argc, char** argv){
     int k, iter; //later check validity
     char *filename;
-    if (argc == 4){
-        int k = atoi(argv[1]); 
-        int iter = atoi(argv[2]); 
+    if ((argc != 3) && (argc != 4)){
+        perror("Invalid Input!");
+        return 1;
+    }
+    else if (argc == 4){
+        k = atoi(argv[1]); 
+        iter = atoi(argv[2]); 
         filename = argv[3];
     }
     else if (argc == 3){
         k = atoi(argv[1]); 
         iter = 200;
         filename = argv[2];
-    }
-    else{
-        perror("Invalid Input!");
-        return 1;
     }
     k_means(k, iter, filename);
     return 0;
