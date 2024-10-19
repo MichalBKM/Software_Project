654970ad4dc598cd98bb96b1de75a58ee1badd3f
Michal Berkheim <128152891+MichalBKM@users.noreply.github.com>
2024-10-19 18:00:17 +0300

    Initialized H in the PYTHON code

--- Final_Project/symnmf.py
+++ Final_Project/symnmf.py
@@ -1,18 +1,21 @@
 import sys
 import math
 import numpy as np
+import symnmf #importing the C-PYTHON API
 
 np.random.seed(1234)
 
-if __name__ == "__main__":
-    k = int(sys.argv[1])
+def main():
+    #Reading user CMD Arguments
+    k = int(sys.argv[1]) #Number of required clusters
     goal = sys.argv[2]
     file_name = sys.argv[3]
 
+#אביב, כאן צריך לקרוא לפונקציות בהתאם למודול
     if goal == 'symnmf':
         #TODO
     elif goal == 'sym':
-        #TODO
+        result = symnmf.sym()
     elif goal == 'ddg':
         #TODO
     elif goal == 'norm':
@@ -20,3 +23,10 @@ if __name__ == "__main__":
     else:
         print("Invalid Goal!")
         exit(1)
+
+def initialize_H(W,k):
+    m = np.mean(W)
+    return np.random.uniform(0, 2*math.sqrt((m/k)), size=(len(W),k))
+
+if __name__ == "__main__":
+    main()
