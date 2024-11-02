479561c480e198ea42d377d5673a109e02fa5094
Aviv Bar Or <92517487+avivbaror@users.noreply.github.com>
2024-11-02 12:49:32 +0200

    Add files via upload

--- Final_Project/analysis.py
+++ Final_Project/analysis.py
@@ -7,6 +7,14 @@ import kmeans
 from sklearn.metrics import silhouette_score
 from symnmf import initialize_H, compute_data_matrix
 
+def mean_cluster(vector, cluster):
+
+    return None
+
+#Retruns the closest centroid for given vector
+def closest_centroid(vector, centroids):
+    distances = np.linalg.norm(centroids - vector, axis=1)
+    return np.argmin(distances)
 
 def main():
     k = int(sys.argv[1])
@@ -21,6 +29,17 @@ def main():
     optimal_H = symnmfmodule.symnmf(W, H, n, k)
 
     cluster_indexes = np.argmax(optimal_H, axis=1)
+    #TODO: Compute centroids
+    #TODO: For each data point, compute the closest centroid to the data point which isn't the cluster its currently in
+
+    sum = 0
+    for row in dataMatrix:
+        #TODO: a = ?
+        #TODO: b = ?
+
+        silhouette_coefficient = (b-a)/max(a, b)
+        sum += silhouette_coefficient
+    
 
 if __name__ == "__main__":
     main()
